3e6112c151f49119c7c3c63829b393d8ec8f9c78
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-20 13:57:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -124,4 +124,24 @@ client.on('message', message => {
   }
 });
 
+module.exports.run = async(bot, message, args) => {
+    let msg = await message.channel.send("doing some magic ...");
+    let target = message.mentions.users.first() || message.author;
+
+    await message.channel.send({files: [
+        {
+            attachment: target.displayAvatarURL,
+            name: "avatar.png"
+        }
+    ]});
+
+    msg.delete();
+}
+
+module.exports.help = {
+    name: "avatar",
+    description: "show the avatar of a user",
+    usage: "[@user]"
+}
+
 client.login(process.env.BOT_TOKEN);
