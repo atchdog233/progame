bf72f1ff4a0b167d0b06572c68f69c59f8d71157
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-22 09:55:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -103,9 +103,15 @@ client.on("message", (message) => {
           message.reply("Invalid user."); //Reply with a mention saying "Invalid user."
     }
  } else
- if (message.content.startsWith("!say")) {
-   message.channel.sendMessage((message.author.username) + " says: " + (message.content.replace('!say ','')));
- };
+  if(command === "/say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
  if (message.content === (prefix + "serverinfo")) {
     message.channel.send({embed: {
       color: 0xffff00,
