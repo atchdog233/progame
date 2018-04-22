e59149937d77c154e94bc30523fe5f896054b703
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-22 10:06:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -152,4 +152,16 @@ client.on('message', message => {
   }
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
