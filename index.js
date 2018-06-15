706cc95c3583b9a386d830033b7a0e2e0d1560e5
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-15 15:47:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity(`/rainbow | Created by Derpy`, {type: "PLAYING"});
+setInterval(function(){
+  bot.guilds.get('451055573103017984').roles.find('name', 'rainbow').edit({color: 'RANDOM'})
+},1000);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
+const prefix = "/rainbow ";
+
+client.on('message', (msg, message) => {
+  if (msg.content === `${prefix}ping`) {
     msg.reply('Pong!');
+  }
+  if (msg.content === `${prefix}creator`) {
+    msg.channel.send('**The creator is:**\n\nDerpy [MIG] ᴰᵉᵛ ⚒#6522');
+  }
+  if (msg.content === `${prefix}help`) {
+    const helpEmbed = new Discord.RichEmbed()
+.setTitle("Rainbow Bot Help Commands")
+.setDescription(`${prefix}ping - check your ping\n${prefix}creator - check who is the creator`)
+.setColor("RANDOM")
+.setFooter("Rainbow Bot by Derpy [MIG] ᴰᵉᵛ ⚒#6522");
+msg.channel.send(helpEmbed);
   }
 });
 
