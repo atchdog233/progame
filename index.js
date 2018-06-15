9c7def5a397a2bb6fe329de7a2670f2623eb1bf4
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-15 15:33:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setStatus("online");
   client.user.setGame("Rainbow Bot by Derpy");
-  setInterval(function(){
-    client.guilds.get('423115512579620865').roles.find('name', 'rainbow').edit({color: 'RANDOM'})
-},1000);
+  //setInterval(function(){
+    //client.guilds.get('423115512579620865').roles.find('name', 'rainbow').edit({color: 'RANDOM'})
+//},1000);
 });
 
 client.on("message", (message) => {
-
-  if (!message.content.startsWith(prefix)) return;
 
   if (message.content === ("creator")) {
 message.channel.send("**The Creator is:**\n\nDerpy [MIG] ᴰᵉᵛ ⚒#6522");
