8badfc075a7ff3da9e0b633adac2b130297d33be
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-15 15:34:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setStatus("online");
-  client.user.setGame("Rainbow Bot by Derpy");
-  //setInterval(function(){
-    //client.guilds.get('423115512579620865').roles.find('name', 'rainbow').edit({color: 'RANDOM'})
-//},1000);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (message) => {
-
-  if (message.content === ("creator")) {
-message.channel.send("**The Creator is:**\n\nDerpy [MIG] ᴰᵉᵛ ⚒#6522");
-      }
-    }});
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
