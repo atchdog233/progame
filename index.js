a80f34309ad645e82970b4136cd83aaf6ba9e8b2
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-15 18:52:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,29 @@ client.on('message', (msg, message) => {
   if (msg.content === `${prefix}creator`) {
     msg.channel.send('**The creator is:**\n\nDerpy [MIG] ᴰᵉᵛ \:hammer_pick:#6522');
   }
+  if (msg.content === `${prefix}rainbow`) {
+  const rainbowrole = args[1]
+  if (!rainbowrole) return msg.channel.send("You need write a **role name** (role mention dont be accepted)")
+  let rainbowembed = new Discord.RichEmbed()
+  .setTitle("Rainbow role Request")
+  .setTimestamp()
+  .addField('Username', message.author.toString())
+  .addField('Server', `${message.guild.name}`)
+  .addField('Message', rainbowrole);
+  msg.delete()
+  let str = "<@!311604263379795970>"; 
+  let id = str.replace(/[<@!>]/g, '');
+
+  client.fetchUser(id).then(user => {user.send(rainbowembed)})
+  msg.reply('Thanks for your request');
+  }
   if (msg.content === `${prefix}invite`) {
     msg.channel.send('**Invite the Bot:**\n\n<https://discordapp.com/oauth2/authorize?client_id=455134292817870848&permissions=8&scope=bot>');
   }
   if (msg.content === `${prefix}help`) {
     const helpEmbed = new Discord.RichEmbed()
     .setTitle("Rainbow Bot Help Commands")
-    .setDescription(`${prefix}ping - check your ping\n${prefix}creator - check who is the creator\n${prefix}invite - Invite the Bot`)
+    .setDescription(`${prefix}ping - check your ping\n${prefix}creator - check who is the creator\n${prefix}invite - Invite the Bot\n${prefix}rainbow [role name] - request for rainbow role on your server (its will takes some days)`)
     .addField("Links", "[Invite the Bot](https://discordapp.com/oauth2/authorize?client_id=455134292817870848&permissions=8&scope=bot)")
     .setColor("RANDOM")
     .setFooter("Rainbow Bot by Derpy [MIG] ᴰᵉᵛ ⚒#6522");
