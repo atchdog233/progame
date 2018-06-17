963b4ce59219dd9f473b3d24091a0d2d32eeb3be
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-17 19:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.user.setActivity(`${prefix}help | Created by Derpy`, {type: "PLAYING"});
     setInterval(function(){
         bot.guilds.get(servers.niv).roles.find('name', 'rainbow').edit({color: 'RANDOM'})},1000);
     setInterval(function(){
-        bot.guilds.get("457892658208178176").roles.find('name', 'rainbow').edit({color: 'RANDOM'})},1000);
+        bot.guilds.get(servers.derpybots).roles.find('name', 'rainbow').edit({color: 'RANDOM'})},1000);
     setInterval(function(){
         bot.guilds.get(servers.poke).roles.find('name', 'rainbow').edit({color: 'RANDOM'})},1000);
     //setInterval(function(){
