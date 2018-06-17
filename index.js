d86b43e8c8b7a906774dccaecc50d1cfa4043444
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-17 09:09:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const Discord = require('discord.js');
+const fs = require("fs"); 
 const bot = new Discord.Client();
 const prefix = "=";
 const servers = require("./servers.json");
+bot.commands = new Discord.Collection();
+let cooldown = new Set();
+let cdseconds = 5;  
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
 
 bot.on('ready', () => {
   console.log("The Bot Online")
@@ -57,11 +78,22 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return 
 
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+
+  let prefix = prefixes[message.guild.id].prefixes;
+  if(!message.content.startsWith(prefix)) return;
+
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (!message.content.startsWith(prefix)) return;
+let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
   if (cmd === `${prefix}ping`) {
     message.channel.send("Pong! `"+`${Date.now() - message.createdTimestamp} ms`+"`");
