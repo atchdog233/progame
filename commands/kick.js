7a88b5233a1745ed1f9ec62040dedbbd4075969b
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-06-29 11:15:13 +0300

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+const Color = require("../Color.json");
+const ID = require("../ID.json");
+const Emoji = require("../Emoji.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!member) return;
+    let kReason = args.join(" ").slice(22);
+    if(!kReason) return;
+    if(!message.member.hasPermission("KICK_MEMBERS")) 
+    if (message.author.id !== ("311604263379795970"))
+    if (message.author.id !== ("362310398864654337")) return;
+
+    message.delete()
+
+    message.guild.member(member).ban(kReason);
+
+module.exports.help = {
+  name:"ban"
+}
