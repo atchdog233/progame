0f6a166b7dc76713c07547b56c67451d15e652cc
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 09:23:34 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
     let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!member) return message.channel.send("תוודא שרשמת שם של שחקן");
     if(!message.member.hasPermission("MANAGE_MEMBERS"))
-    if (message.author.id !== ("311604263379795970")) return message.channel.send("");
-    if(member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("אתה לא יכול לבצע את הפקודה על השחקן הזה");
+    if (message.author.id !== ("311604263379795970")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה");
+    if(member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה על השחקן הזה");
     let reason = args.join(" ").slice(22);
     if(!reason) return message.channel.send("תוודא שרשמת סיבה")
 
