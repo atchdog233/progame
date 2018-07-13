36e8bf949e5c554f1592fb1edfb09844fab01a1b
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:55:56 +0300

    Update credit.js

--- commands/credit.js
+++ commands/credit.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
-
+    message.channel.send('**:יוצר הבוט הוא**\n\n`Derpy [MIG] ᴰᵉᵛ ⚒#6522`');
 }
 module.exports.help = {
     name:"credit"
