b7d7fa501031ef75d084ac3563f8b59eb065a33a
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 18:37:55 +0300

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
   if (message.author.id !== ("311604263379795970")) return;
-  bot.user.setGame(args[0]);
+  bot.user.setStatus(args[0]);
   message.channel.send(`${args[0]} סטטוס של הבוט השתנה ל`)
 }
 module.exports.help = {
