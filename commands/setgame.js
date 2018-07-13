9e5a8d2a97daf334adf007a671f9f224e833749f
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:54:28 +0300

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
-
+  if(!message.member.hasPermission("MANAGE_GUILD"))
+  if (message.author.id !== ("311604263379795970")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה");
+  bot.user.setGame(message.content.split(' ').slice(1).join(' '));
+  message.channel.send("`"+message.content.split(' ').slice(1).join(' ')+"`הבוט עכשיו משחק ב")
 }
 module.exports.help = {
     name:"setgame"
