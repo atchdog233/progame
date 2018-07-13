8f3bde539015208de5cc0e99b6cc2db4a8cdda4d
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 09:19:22 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,24 +8,17 @@ module.exports.run = async (bot, message, args) => {
     if (message.author.id !== ("311604263379795970")) return message.channel.send("");
     if(member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("אתה לא יכול לבצע את הפקודה על השחקן הזה");
     let reason = args.join(" ").slice(22);
-    if(!reason) return ("תוודא שרשמת סיבה")
-
-    let warnEmbed = new Discord.RichEmbed()
-    .setDescription("אזהרה")
-    .setColor("RANDOM")
-    .addField("שחקן", `${member}`)
-    .addField("צוות", `${message.author}`)
-    .addField("סיבה", reason)
+    if(!reason) return message.channel.send("תוודא שרשמת סיבה")
 
     message.delete().catch(O_o=>{});
-    message.channel.send(`**${member.user.username}#${member.user.discriminator} קיבל אזהרה**`);
+    message.channel.send(`**קיבל אזהרה ${member.user.username}#${member.user.discriminator}**`);
 
     let str = `<@!${member.id}>`; 
 
     let id = str.replace(/[<@!>]/g, '');
 
     bot.fetchUser(id)
-    .then(user => {user.send(`${reason} בגלל ${message.guild.name} קיבלת אזהרה בשרת`)})
+    .then(user => {user.send(`${reason} בגלל **${message.guild.name}** קיבלת אזהרה בשרת`)})
 
   }
 
