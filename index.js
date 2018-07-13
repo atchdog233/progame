9905342d37cf1df2d213852376ccef60d3d35255
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:45:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,25 +18,6 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
   let command = message.content.split(' ').slice(0).join(' ')
 
-  if (cmd === `${prefix}serverinfo`) {
-    let embed = new Discord.RichEmbed()
-    .setDescription("מידע על השרת")
-    .setColor(`RANDOM`)
-    .addField("שם השרת", message.guild.name)
-    .addField("של השרת ID", `${message.guild.id}`)
-    .addField("מנהל השרת", message.guild.owner.user.tag)
-    .addField("איזור", message.guild.region)
-    .addField("מספר שחקנים ובוטים", `${message.guild.memberCount}`)
-    .addField("מספר שחקנים", `${message.guild.members.filter(member => !member.user.bot).size}`)
-    .addField("מספר בוטים", `${message.guild.members.filter(member => member.user.bot).size}` || "לא מצאתי שום בוט")
-    .addField("מספר רולים", message.guild.roles.size || "לשרת אין שום רול")
-    .addField("השרת נוצר ב", `${message.guild.createdAt}`)
-    .addField("אתה הצטרפת לשרת ב", `${message.member.joinedAt}`)
-    .setTimestamp()
-    .setFooter(`${bot.user.username}`);
-
-    message.channel.send(embed);
-  }
   if (cmd === `${prefix}userinfo`) {
 	let user;
     if (message.mentions.users.first()) {
