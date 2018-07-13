9130e2869d047f1f174a7fda77146fee61addd86
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:53:52 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,23 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
-
+  var member = message.mentions.users.first();
+  var x5bzm;
+    if(!member) {
+    }
+    if(member){
+        var x5bzm = member;
+    } else {
+        var x5bzm = message.author;
+        
+    }
+      const embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setTitle(`${x5bzm.username}#${x5bzm.discriminator}`)
+      .setImage(`${x5bzm.avatarURL}`)
+      .setTimestamp()
+      .setFooter(`${bot.user.username}`);
+    message.channel.sendEmbed(embed);
 }
 module.exports.help = {
     name:"avatar"
