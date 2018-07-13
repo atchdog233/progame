5ebb6b489a0e53cc509e1fc77e5f27f543d7d7c7
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:42:44 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
-
+message.channel.send("`"+`${bot.ping}`+"`ms :הפינג שלך הוא");
 }
 module.exports.help = {
     name:"ping"
