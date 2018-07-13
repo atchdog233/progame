fdd9ec27f6a7ae7ce088833c02bc6a12191f830a
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:55:14 +0300

    Update resetgame.js

--- commands/resetgame.js
+++ commands/resetgame.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
-
+  if(!message.member.hasPermission("MANAGE_GUILD"))
+  if (message.author.id !== ("311604263379795970")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה");
+  bot.user.setGame(`${prefix}help | ProGame Israel Community`);
+  message.channel.send("משחק הבוט שונה חזרה למשחק הרגיל")
 }
 module.exports.help = {
     name:"resetgame"
