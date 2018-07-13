2c3b591feea8961b82919d3263e0233e1de368c1
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:45:44 +0300

    Update and rename server.js to serverinfo.js

--- commands/server.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-
-module.exports.run = async (bot, message, args) => {
-message.channel.send("`"+`${bot.ping}`+"`ms :הפינג שלך הוא");
-}
-module.exports.help = {
-    name:"serverinfo"
-  }
