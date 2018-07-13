eded89756c9a25d711d6fa1e6daa1792d83aebe0
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:51:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,59 +18,6 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
   let command = message.content.split(' ').slice(0).join(' ')
 
-  if (cmd === `${prefix}userinfo`) {
-	let user;
-    if (message.mentions.users.first()) {
-        user = message.mentions.users.first();
-        //joineddiscord = (user.createdAt.getDate() + 1) + '-' + (user.createdAt.getMonth() + 1) + '-' + user.createdAt.getFullYear() + ' | ' + user.createdAt.getHours() + ':' + user.createdAt.getMinutes() + ':' + user.createdAt.getSeconds();
-        //joinedserver = (user.joinedAt.getDate() + 1) + '-' + (user.joinedAt.getMonth() + 1) + '-' + user.joinedAt.getFullYear() + ' | ' + user.joinedAt.getHours() + ':' + user.joinedAt.getMinutes() + ':' + user.joinedAt.getSeconds();
-
-    } else {
-        user = message.author;
-    }
-    if (user.presence.status === 'online') {
-        user.presence.status = '**מחובר**';
-    } else if (user.presence.status === 'dnd') {
-        user.presence.status = '**נע לא להפריע**';
-    } else if (user.presence.status === 'idle') {
-        user.presence.status = '**לא נמצא**';
-    } else if (user.presence.status === 'offline') {
-        user.presence.status = '**מנותק**';
-    } 
- 
-if (args == '') {
-var z = message.author
-} else {
-var z = message.mentions.users.first();
-}
-
-let d = z.createdAt;          
-let n = d.toLocaleString();   
-let x;                       
-let y;                        
-
-if (z.bot) {
-var type = 'בוט';
-}else {
-var type = 'ממבר';
-}
-    const member = message.guild.member(user);
-
-    const embed = new Discord.RichEmbed()
-		.setColor(`RANDOM`)
-		.setTitle(`${user.username}#${user.discriminator}`)
-		.addField("של השחקן ID", `${user.id}`)
-		.addField("השם כינוי של השחקן", `${member.nickname !== null ? `${member.nickname}` : 'אין שם כינוי'}`)
-                .addField("יצר את החשבון ב", `${user.createdAt}`)
-		.addField("הצטרף לשרת ב", `${message.member.joinedAt}`)
-		.addField("סוג משתמש", `**`+type+`**`)
-                .addField("סטטוס של השחקן", `${user.presence.status}`)
-		.addField("משחק של השחקן", `${user.presence.game ? user.presence.game.name : 'הוא כרגע לא משחק'}`)
-		.addField("הרולים של השחקן", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `<@&${roles.id}>`).join(" ") || "לשחקן אין שום רול"}`)
-                .setTimestamp()
-                .setFooter(`${bot.user.username}`);
-     message.channel.send({embed})
-  }
   if (cmd === `${prefix}clear`) {
     if(!args[0]) return message.channel.send("תוודא שרשמת את כמות ההודעות שאתה רוצה למחוק");
   if(!message.member.hasPermission("MANAGE_MESSAGES")) 
