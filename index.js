b53c6de1571aa0f86391032c0f59bcf442ca056a
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-07-13 08:52:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,22 +18,6 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
   let command = message.content.split(' ').slice(0).join(' ')
 
-  if (cmd === `${prefix}clear`) {
-    if(!args[0]) return message.channel.send("תוודא שרשמת את כמות ההודעות שאתה רוצה למחוק");
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) 
-  if (message.author.id !== ("311604263379795970")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`ניקה ${args[0]} הודעות`).then(msg => msg.delete(5000));
-  });
-  }
-  if (cmd === `${prefix}purge`) {
-    if(!args[0]) return message.channel.send("תוודא שרשמת את כמות ההודעות שאתה רוצה למחוק");
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) 
-  if (message.author.id !== ("311604263379795970")) return message.channel.send("אין לך מספיק גישות לבצע את הפקודה הבאה");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`ניקה ${args[0]} הודעות`).then(msg => msg.delete(5000));
-  });
-  }
   if (cmd === `${prefix}avatar`) {
   var member = message.mentions.users.first();
   var x5bzm;
